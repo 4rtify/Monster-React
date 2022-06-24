8d2cfb0cbb2b7bfff0420b472230db8ffb297481
Nikolay Miroshnichenko <nikolai.miroshnichenko@gmail.com>
2022-06-24 18:34:51 +1000

    lets get this started

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState, ChangeEvent } from "react";
+
+import { getData } from "./utils/data.utils";
+import "./App.scss";
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filtermonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setMonsters(users);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="App">
+      <input className='search-box' type="search" placeholder="monster" onChange={(event) => {}}/>
+      {monsters.map((monster) => {
+        return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
